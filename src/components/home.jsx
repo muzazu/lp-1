11cae548a43ead99b012bbb9fb80a8c9f1375885
Muhammad Zaid <richardmmeans322@gmail.com>
2020-06-06 16:01:26 +0700

    features/eslint-config added prettier & eslint

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+function Home() {
+    return <div className="home"></div>
 }
 
-export default App;
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Home from "./components/home";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/home'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
