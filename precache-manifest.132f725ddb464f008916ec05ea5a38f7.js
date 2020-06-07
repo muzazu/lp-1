43a0faca4f637154a608c0c56cb64f84db77841a
Muhammad Zaid <richardmmeans322@gmail.com>
2020-06-07 17:17:53 +0700

    Updates

---
+++ precache-manifest.132f725ddb464f008916ec05ea5a38f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0f339997f16eb79c4fddb933d3e3d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "063501969f3dcbf0dac7",
+    "url": "/static/css/main.e8714c91.chunk.css"
+  },
+  {
+    "revision": "da78ccb0837f7bc3acfd",
+    "url": "/static/js/2.432713aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.432713aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063501969f3dcbf0dac7",
+    "url": "/static/js/main.84d32afa.chunk.js"
+  },
+  {
+    "revision": "9c748938312623cbad40",
+    "url": "/static/js/runtime-main.e861fc41.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/static/media/work-desk__dustin-lee.a5a74b4f.jpg"
+  }
+]);
